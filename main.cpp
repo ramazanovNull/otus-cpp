7ec3cd6e979255edc782f3b53969d79cccb5d86f
Ramazanov Null <ramazanov.null@gmail.com>
2021-10-05 19:13:36 +0300

    change 2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 
 int main (int, char **) {
     std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, Abakar!" << std::endl;
+    std::cout << "Hello, Abba!" << std::endl;
     return 0;
 }
